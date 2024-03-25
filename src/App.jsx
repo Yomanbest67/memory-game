9fc11a9129b4a9aa7f5f87c72d797206fa65a781
Yomanbest <106496623+Yomanbest67@users.noreply.github.com>
2024-03-25 21:08:14 +0000

    Functionality done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import {v4 as uuidv4} from 'uuid';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [clicked, setClicked] = useState([]);
+  const [pokemonDiv, setPokemonDiv] = useState([]);
+  const [pokemonList, setPokemonList] = useState(['pikachu', 'ditto', 'bulbasaur', 
+    'charmander', 'squirtle', 'meowth', 'geodude', 'raichu',
+    'pidgey', 'rattata', 'zubat', 'jigglypuff'
+  ]);
+  const [random, setRandom] = useState(false);
+
+  // function randomizePokemon(pokemonlist) {
+  //     let randomized = pokemonlist.sort(() => Math.random() - 0.5);
+
+  //     setPokemonList(randomized);
+  //     random? setRandom(false) : setRandom(true);
+  // }
+
+  useEffect(() => {
+      pokemonList.forEach((pokemon) => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+          .then((result) => result.json())
+          .then((data) => {
+            setPokemonDiv((prevPokemonDiv) => {
+              return [
+                ...prevPokemonDiv,
+                {
+                  name: pokemon,
+                  image: data.sprites.front_default,
+                  id: uuidv4(),
+                },
+              ];
+            });
+          });
+      });
+      
+  }, [pokemonList, random]);
+
+  function handleClick(event, pokemon) {
+    if (clicked.includes(pokemon)) {
+      setBestScore(score > bestScore ? score : bestScore);
+      setScore(0);
+
+      clicked.splice(0, clicked.length);
+    } else {
+      
+      setClicked([...clicked, pokemon]);
+      setScore(score + 1);
+    }
+
+    // randomizePokemon(pokemonList);
+
+    shuffleCards(pokemonDiv);
+  }
+
+  function shuffleCards(list) {
+    list = list.sort(() => Math.random() - 0.5);
+
+    return (
+      list.map((pokemon) => (
+        <div key={pokemon.id} className='pokemon' onClick={(event) => handleClick(event, pokemon.name)}>
+          <img src={pokemon.image} alt={pokemon.name} />
+          <h3>{pokemon.name}</h3>
+        </div>
+      ))
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>Pokemon Memory Game</h1>
+        <h2>Score: {score}</h2>
+        <h2>Best Score: {bestScore}</h2>
+        <div className='pokemon-container'>
+          {shuffleCards(pokemonDiv)}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
